0c0ac684cffb62215e0fd5ffd9da0c9cefcd17d0
Standard User <wendy.hardianto@gmail.com>
2017-09-04 09:13:40 +0700

    Handling error for network connection state

--- app/src/main/java/com/example/standard/bakingapp/app/ActivityMain.java
+++ app/src/main/java/com/example/standard/bakingapp/app/ActivityMain.java
@@ -1,12 +1,18 @@
 package com.example.standard.bakingapp.app;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.standard.bakingapp.R;
 import com.example.standard.bakingapp.backend.pojo.Recipe;
@@ -23,7 +29,9 @@ public class ActivityMain extends AppCompatActivity
     implements AdapterRecipeList.clickHandler {
 
   private static final String PARCEL_TAG = "RECIPE";
+
   private RecyclerView rvwListRecipe;
+  private CoordinatorLayout vwCoordinator;
 
   @Override
   public void onClickAdapterRecipeList(Recipe obj) {
@@ -41,38 +49,58 @@ public class ActivityMain extends AppCompatActivity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    vwCoordinator = (CoordinatorLayout) findViewById(R.id.main_coordinator);
+
     rvwListRecipe = (RecyclerView) findViewById(R.id.content_recipe_list_frame);
     rvwListRecipe.setHasFixedSize(true);
 
+    //Handling Network Is Connected or Not
+    ConnectivityManager connectivityManager =
+        (ConnectivityManager) getApplicationContext().getSystemService(CONNECTIVITY_SERVICE);
+    NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+
+    boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+    if(isConnected) {
     /* Fetch API Content Here */
-    Fetcher theFetcher = new Fetcher();
-    APIEndpoint theEndpoint = theFetcher.getFetcher().create(APIEndpoint.class);
+      Fetcher theFetcher = new Fetcher();
+      APIEndpoint theEndpoint = theFetcher.getFetcher().create(APIEndpoint.class);
 
-    Call<List<Recipe>> callListRecipe = theEndpoint.getListRecipe("android-baking-app-json");
+      Call<List<Recipe>> callListRecipe = theEndpoint.getListRecipe("android-baking-app-json");
 
-    callListRecipe.enqueue(new Callback<List<Recipe>>() {
-      @Override
-      public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
-        List<Recipe> listRecipe = response.body();
+      callListRecipe.enqueue(new Callback<List<Recipe>>() {
+        @Override
+        public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+          List<Recipe> listRecipe = response.body();
 
-        if (listRecipe != null) {
-          AdapterRecipeList adpRecipeList = new AdapterRecipeList(listRecipe);
-          adpRecipeList.setOnCardViewClick(ActivityMain.this);
+          if (listRecipe != null) {
+            AdapterRecipeList adpRecipeList = new AdapterRecipeList(listRecipe);
+            adpRecipeList.setOnCardViewClick(ActivityMain.this);
 
-          if (getApplicationContext().getResources().getBoolean(R.bool.should_use_multi_grid)) {
-            rvwListRecipe.setLayoutManager(new GridLayoutManager(getApplicationContext(), 3));
-          } else {
-            rvwListRecipe.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            if (getApplicationContext().getResources().getBoolean(R.bool.should_use_multi_grid)) {
+              rvwListRecipe.setLayoutManager(new GridLayoutManager(getApplicationContext(), 3));
+            } else {
+              rvwListRecipe.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            }
+            rvwListRecipe.setAdapter(adpRecipeList);
           }
-          rvwListRecipe.setAdapter(adpRecipeList);
         }
-      }
 
-      @Override
-      public void onFailure(Call<List<Recipe>> call, Throwable t) {
-        Log.e("RECIPE", "onFailure: " + t.toString());
-      }
-    });
+        @Override
+        public void onFailure(Call<List<Recipe>> call, Throwable t) {
+          showSnackbar(t.toString());
+        }
+      });
+    }
+    else {
+      showSnackbar(getString(R.string.activity_main_no_internet));
+    }
     /* End of Fetch API Content */
+  }
+
+  private void showSnackbar(String theMessage) {
+    Snackbar
+      .make(vwCoordinator, theMessage, Snackbar.LENGTH_LONG)
+      .show();
   }
 }
