3eb85cbf0b84494db45e61d299dc830c9c403d59
Standard User <wendy.hardianto@gmail.com>
2017-09-05 09:28:18 +0700

    Adding private constructor for StaticValue

--- app/src/main/java/com/example/standard/bakingapp/app/StaticValue.java
+++ app/src/main/java/com/example/standard/bakingapp/app/StaticValue.java
@@ -4,10 +4,12 @@ package com.example.standard.bakingapp.app;
  * Created by standard on 8/21/17.
  */
 
-class StaticValue {
+final class StaticValue {
   static final String KEY_OBJECT_RECIPE = "RECIPE";
   static final String KEY_OBJECT_RECIPESTEP = "STEP";
   static final String KEY_STRING_RECIPEINGREDIENT = "INGREDIENT";
   static final String KEY_INT_POSITION_CURR = "POSITION_CURR";
   static final String KEY_INT_POSITION_MAX = "POSITION_MAX";
+
+  private StaticValue() {}
 }
